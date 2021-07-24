392c6175a3a9ec4156a145f7b049e74ae1be90f9
TomasGintautas <tomasgintaut@gmail.com>
2021-07-24 16:02:39 +0300

    set up design templates for thymeleaf implementation

--- src/main/java/lt/codeacademy/blogproject/utils/AccessRightValidation.java
+++
@@ -1,9 +0,0 @@
-package lt.codeacademy.blogproject.utils;
-
-public class AccessRightValidation {
-
-    //TODO: Checks if admin - to delete / create / update articles and delete comments.
-    // Checks if user - to create / delete / update comments
-    // else - guest
-    // bet ar sitas ne thymeleafe?
-}
