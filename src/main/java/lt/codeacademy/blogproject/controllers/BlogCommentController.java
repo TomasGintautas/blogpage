29437e02916f42f8be63a8f8cff6086af23a8e0f
TomasGintautas <tomasgintaut@gmail.com>
2021-08-01 17:37:52 +0300

    minor tweaks

--- src/main/java/lt/codeacademy/blogproject/controllers/BlogCommentController.java
+++ src/main/java/lt/codeacademy/blogproject/controllers/BlogCommentController.java
@@ -16,21 +16,18 @@ public class BlogCommentController {
     @Autowired
     private BlogCommentService blogCommentService;
 
-    @PreAuthorize("hasRole('USER') || principal.username == #comments.creator")
     @PostMapping(value = "/comment/{id}/edit")
     public String updateBlogComment(@Valid BlogCommentRequest blogCommentRequest){
         blogCommentService.updateBlogComment(blogCommentRequest);
         return "redirect:/";
     }
 
-    @PreAuthorize("hasRole('USER') || principal.username == #comments.creator")
     @GetMapping(value = "/comment/{id}/edit")
     public String updateBlogComment(@PathVariable("id") Long id, Model model){
         model.addAttribute("comment", blogCommentService.getOneComment(id));
         return "/comment/edit";
     }
 
-    @PreAuthorize("hasRole('ADMIN') || principal.username == #comments.creator")
     @GetMapping(value = "/comment/{id}/delete")
     public String deleteBlogComment(@PathVariable("id") Long id){
         blogCommentService.deleteBlogComment(id);
