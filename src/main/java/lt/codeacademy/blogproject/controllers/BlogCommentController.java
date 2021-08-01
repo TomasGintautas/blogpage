849ae44893d4f587a41383ea8360399dd6c4993c
TomasGintautas <tomasgintaut@gmail.com>
2021-08-01 17:39:18 +0300

    minor tweaks

--- src/main/java/lt/codeacademy/blogproject/controllers/BlogCommentController.java
+++ src/main/java/lt/codeacademy/blogproject/controllers/BlogCommentController.java
@@ -3,7 +3,6 @@ package lt.codeacademy.blogproject.controllers;
 import lt.codeacademy.blogproject.controllers.dto.BlogCommentRequest;
 import lt.codeacademy.blogproject.services.BlogCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
